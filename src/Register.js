d529f7102f4a3965c2d0618f7c1a5b8d5ce11948
Ginver <74971813+Ginver@users.noreply.github.com>
2023-04-18 16:47:48 +0200

    Register page added

---
+++ src/Register.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Register.css'
+
+const Register = () => {
+
+    return (
+        <div>
+            alert("Hello future user!")
+        </div>
+    )
+
+}
+
+export default Register;
