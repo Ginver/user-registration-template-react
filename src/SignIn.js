49855e89324854855c618268bfc56c192e830501
Ginver <74971813+Ginver@users.noreply.github.com>
2023-04-19 11:40:03 +0200

    SignIn page basic setup

---
+++ src/SignIn.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './SignIn.css'
+
+const SignIn = () => {
+
+    return (
+        <>
+            <h3>
+                Hallo there again!
+            </h3>
+
+            <div className="signin-container">
+
+                <label>username:
+                    <input />
+                </label>
+
+                <label>password:
+                    <input />
+                </label>
+            </div>
+
+        </>
+
+    )
+
+}
+
+export default SignIn;
